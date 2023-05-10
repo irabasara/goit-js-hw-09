55c349884f735484e99e9c7330c9f04fadd7ee4e
irabasara <125157770+irabasara@users.noreply.github.com>
2023-05-10 19:46:53 +0000

    Deploying to gh-pages from @ irabasara/goit-js-hw-09@ce7790cb650b335979ed60b4dbf4c03cabd9771c 🚀

--- 01-color-switcher.620d2630.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let a=null;t.addEventListener("click",(function(){a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,t.setAttribute("disabled","disabled"),e.removeAttribute("disabled","disabled")}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.removeAttribute("disabled","disabled"),e.setAttribute("disabled","disabled")}));
-//# sourceMappingURL=01-color-switcher.620d2630.js.map
